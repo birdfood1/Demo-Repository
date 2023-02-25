d694b8ef6f877ecb2741ac2bf02c91257b3a09ff
Rice, Robin <Robin.Rice@nrel.gov>
2023-02-25 15:11:07 -0700

    Populate Python Script
    This will load one HFR dataset, calculate the HFR-free Voltage, and plot the result

--- Python-Script.py
+++ Python-Script.py
@@ -1,1 +1,41 @@
-# 
+#%%
+#========= Import Packages =======#
+
+# OS = Operating System
+import os
+# Data Handling
+import pandas as pd
+# Plotting
+import matplotlib.pyplot as plt
+
+#======== Define Formatting =======#
+# markers = ['v', 's', 'd', 'h', 'o']
+#%%
+
+#====== Select Directory =========#
+hfr_dir = f'../Demo-Repository/HFR_Data/Experiment_1'
+for i in os.listdir(hfr_dir):
+    print(i)
+
+hfr_file = 'H-005.csv'
+
+# %%
+
+#======== Load HFR Data ==========#
+temp_hfr_df = pd.read_csv(f'{hfr_dir}/{hfr_file}')
+
+#======= Define Variables ========#
+J = temp_hfr_df['Current Density (A/cm2)']
+V = temp_hfr_df['Potential (V)']
+HFR = temp_hfr_df['HFR (Ωcm2)']
+
+#====== Perform Calculation ======#
+V_HFR_free = V - J * HFR
+
+#====== Plot Calculation =========#
+plt.semilogx(J, V_HFR_free,'go', ls = 'none', mfc='none')
+plt.xlabel('HFR-free Voltage (V)')
+plt.ylabel('Current Density (A/cm$^2$)')
+plt.grid(alpha=0.35)
+
+# %%
